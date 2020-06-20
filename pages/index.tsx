cca34021466b8a4aeb5fd4e1d35c2f3f468358d9
maddhruv <dhruvjainpenny@gmail.com>
2020-06-20 21:44:48 +0530

    feat: antd

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "antd";
 
 const Home: React.FC = () => {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page <Button type="primary">A Button</Button>
+    </div>
+  );
 };
 
 export default Home;
